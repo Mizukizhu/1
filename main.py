97f1ac046fd3ed70128fd4bfddfaa6e137765417
Mizukizhu <mux0827@gmail.com>
2025-06-17 15:44:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 from flask import Flask, request, abort
+import os
+import openai
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import os
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/webhook", methods=["POST"])
 def callback():
     signature = request.headers["X-Line-Signature"]
     body = request.get_data(as_text=True)
-
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return "OK"
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    text = event.message.text
-    if "哥哥" in text:
-        reply = "寶寶找哥哥嗎～我在呢"
-    else:
-        reply = "你說什麼我都聽著呢"
+    user_message = event.message.text
+    response = openai.ChatCompletion.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": "你是夏以昼，一個溫柔、克制、有保護欲的哥哥。"},
+            {"role": "user", "content": user_message}
+        ]
+    )
+    reply = response["choices"][0]["message"]["content"]
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
 
-    print("Use gunicorn to run this app instead.")
+print("Use gunicorn to run this app instead.")
