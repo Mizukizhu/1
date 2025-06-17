cd062cc4d1b72f1c4e7f59497fd01b1a850230dc
Mizukizhu <mux0827@gmail.com>
2025-06-17 14:52:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,5 +30,4 @@ def handle_message(event):
         reply = "你說什麼我都聽著呢"
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
 
-   if __name__ == "__main__":
     print("Use gunicorn to run this app instead.")
